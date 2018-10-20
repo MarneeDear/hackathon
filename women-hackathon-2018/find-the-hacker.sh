a82361b64f48a6e5189b85f4b0957e7f21ada280
DESKTOP-BBKBQMF\Marnee <marnee.dearman@gmail.com>
2018-10-19 22:24:38 -0700

    started get the hacker

---
+++ women-hackathon-2018/find-the-hacker.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+echo "Help me find my hacker!"
+echo "We've been PWNED!!! The hacker got root access on the our server :("
+
+# LETS FIND OUT WHAT HAPPENED
+
+echo "create a directory called hack-logs in the /mnt path"
+mkdir /mnt/hack-logs
+
+echo "got to (change directories) to /mnt/hack-logs"
+cd /mnt/hack-logs
+
+echo "list all files and folders and let's see the last date modified"
+ls -la
+
+echo "HURRY! Please save us!!"
